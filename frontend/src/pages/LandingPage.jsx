5b17849ef34de22e9b6e9441c50731de4d0a8b3c
Wilson Yin <wyin6610@gmail.com>
2024-08-28 03:03:36 -0700

    landing page refactoring

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
-import HomeBanner from '../components/HomeBanner'
+import styles from "../css/landing.module.css"
 
 const LandingPage = () => {
   return (
-    <div>
-      <HomeBanner />
-    </div>
+    <>
+      <div className={styles.landingBanner}>
+        <div className={styles.navBar}>
+          <div className={styles.leftNav}>
+            <h1>Flower Finder</h1>
+          </div>
+
+          <div className={styles.rightNav}>
+            <h2>Upload</h2>
+            <h2>FAQ</h2>
+            <h2 className={styles.login}>Log in</h2>
+          </div>
+        </div>
+
+        <div className={styles.bannerText}>
+          <p>The best free, open-source flower photos</p>
+        </div>
+
+        <div className={styles.inputSearch}>
+          <input type="text" value="Search flower photos" />
+          <button></button>
+        </div>
+      </div>    
+    </>
   )
 }
 
